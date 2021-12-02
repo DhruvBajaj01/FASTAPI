21c0042f07da20abf7e25dfa68280c981ceb1b28
Dhruv Bajaj <9653dhr@gmail.com>
2021-12-02 13:04:07 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -67,7 +67,7 @@ def create_posts(post: Post):
 
 @app.get("/posts/{id}")
 def get_post(id : int,response : Response):
-    cursor.execute("""SELECT * from posts where id = %s""", (str(id)))
+    cursor.execute("""SELECT * from posts where id = %s""", (str(id),))
     post = cursor.fetchone()
     
     if not post :
@@ -77,20 +77,21 @@ def get_post(id : int,response : Response):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id :int):
-    index = find_index_post(id)
-    if index== None:
+    
+    cursor.execute("""DELETE FROM posts WHERE id =%s returning *""",(str(id),))
+    delete_post = cursor.fetchone()
+    conn.commit()
+    if delete_post== None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id :{id} doesnot exist.")
-    my_posts.pop(index)
+    
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 def update_post(id:int, post: Post):
-    index = find_index_post(id)
-    
-    if index== None:
+    cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id =%s returning *""",(post.title,post.content,post.published,str(id)))
+    updated_post=cursor.fetchone()
+    conn.commit()
+    if updated_post== None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id :{id} doesnot exist.")
-   
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"data":post_dict}
+
+    return {"data":updated_post}
