2bdb497547a933cab2061edca153e109e4655ad7
Dhruv Bajaj <9653dhr@gmail.com>
2021-12-02 20:49:48 +0530

    update

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ import time
 from sqlalchemy.orm import Session
 
 
-from . import models
+from . import models,schemas
 from .database import engine,get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -17,10 +17,7 @@ models.Base.metadata.create_all(bind=engine)
 app=FastAPI()
 
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published : bool = True
+
     
 
 while True:
@@ -54,10 +51,6 @@ def find_index_post(id):
 def root():
     return{"message":"Welcome to API"}
 
-@app.get("/sqlalchemy")
-def test_posts(db: Session = Depends(get_db)):
-    posts = db.query(models.Post).all()
-    return {"data": posts}
 
 
 
@@ -72,7 +65,7 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     
     # cursor.execute("""INSERT INTO posts(title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
@@ -112,7 +105,7 @@ def delete_post(id :int,db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id:int, updated_post: Post,db: Session = Depends(get_db)):
+def update_post(id:int, updated_post: schemas.PostCreate,db: Session = Depends(get_db)):
 
     # cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id =%s returning *""",(post.title,post.content,post.published,str(id)))
     # updated_post=cursor.fetchone()
---
+++ app/schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published : bool = True
+
+class PostCreate(PostBase):
+    pass
