32970cbd3532cb831f64056013870ca3ea2bcbea
Dhruv Bajaj <9653dhr@gmail.com>
2021-12-02 15:31:31 +0530

    updaates

--- app/main.py
+++ app/main.py
@@ -17,8 +17,6 @@ models.Base.metadata.create_all(bind=engine)
 app=FastAPI()
 
 
-
-
 class Post(BaseModel):
     title: str
     content: str
@@ -58,54 +56,75 @@ def root():
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    return {"Status":"Succesfull"}
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts=cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts=cursor.fetchall()
+    posts = db.query(models.Post).all()
 
     return {"data": posts}
 
 
 
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts(title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    
+    # cursor.execute("""INSERT INTO posts(title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+
+    new_post =models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data": new_post}
 
 
 @app.get("/posts/{id}")
-def get_post(id : int,response : Response):
-    cursor.execute("""SELECT * from posts where id = %s""", (str(id),))
-    post = cursor.fetchone()
-    
+def get_post(id : int,db: Session = Depends(get_db)):
+
+    # cursor.execute("""SELECT * from posts where id = %s""", (str(id),))
+    # post = cursor.fetchone()
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id : {id} was not found")
         
     return {"post_details": post} 
 
+
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id :int):
+def delete_post(id :int,db: Session = Depends(get_db)):
     
-    cursor.execute("""DELETE FROM posts WHERE id =%s returning *""",(str(id),))
-    delete_post = cursor.fetchone()
-    conn.commit()
-    if delete_post== None:
+    # cursor.execute("""DELETE FROM posts WHERE id =%s returning *""",(str(id),))
+    # delete_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if post.first()== None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id :{id} doesnot exist.")
-    
+    post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}")
-def update_post(id:int, post: Post):
-    cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id =%s returning *""",(post.title,post.content,post.published,str(id)))
-    updated_post=cursor.fetchone()
-    conn.commit()
-    if updated_post== None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id :{id} doesnot exist.")
 
-    return {"data":updated_post}
+@app.put("/posts/{id}")
+def update_post(id:int, updated_post: Post,db: Session = Depends(get_db)):
+
+    # cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id =%s returning *""",(post.title,post.content,post.published,str(id)))
+    # updated_post=cursor.fetchone()
+    # conn.commit()
+    post_query =db.query(models.Post).filter(models.Post.id ==id)
+    
+    post =post_query.first()
+    
+    if post== None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id :{id} doesnot exist.")
+    
+    post_query.update(updated_post.dict(), synchronize_session =False)
+
+    db.commit()
+    return {"data":post_query.first()}
--- app/models.py
+++ app/models.py
@@ -9,5 +9,5 @@ class Post(Base):
     id=Column(Integer, primary_key=True,nullable=False)
     title=Column(String,nullable=False)
     content= Column(String,nullable=False)
-    Published = Column(Boolean,server_default="True",nullable=False)
+    published = Column(Boolean,server_default="True",nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
