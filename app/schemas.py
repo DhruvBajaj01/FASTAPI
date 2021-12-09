ecf9aec9e3d5b7bdb3f93d514e777f7f5e137a02
Dhruv Bajaj <9653dhr@gmail.com>
2021-12-09 12:25:40 +0530

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel,EmailStr
 from datetime import datetime
+from typing import  Optional
 
 from app.database import Base
 
@@ -32,4 +33,9 @@ class UserLogin(BaseModel):
     email : EmailStr
     password : str
 
+class Token(BaseModel):
+    access_token : str
+    token_type: str
 
+class TokenData(BaseModel):
+    id: Optional[str] = None
