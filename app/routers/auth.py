ad28ef51b965df14b7a642155c0903a662a3780f
Dhruv Bajaj <9653dhr@gmail.com>
2021-12-09 12:25:29 +0530

    Update auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -10,7 +10,7 @@ router = APIRouter(
     tags  = ['Authentication']
 )
 
-@router.post('/login')
+@router.post('/login', response_model=schemas.Token)
 def login(user_cred: OAuth2PasswordRequestForm = Depends(),db: Session = Depends(database.get_db)):
     
     
