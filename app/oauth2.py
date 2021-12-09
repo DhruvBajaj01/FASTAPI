c2f6e612287f116764dff94d241795b9e48bf634
Dhruv Bajaj <9653dhr@gmail.com>
2021-12-09 12:49:32 +0530

    Update oauth2.py

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,9 +1,9 @@
 from fastapi import Depends, status, HTTPException
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from .import schemas
+from .import schemas, database, models
 from fastapi.security import OAuth2PasswordBearer
-
+from sqlalchemy.orm import Session
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 #secret_key 
@@ -40,6 +40,9 @@ def verify_access_token(token:str, credentials_exception):
     
     return token_data
 
-def get_current_user(token :str = Depends(oauth2_scheme)):
+def get_current_user(token :str = Depends(oauth2_scheme), db: Session =Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail =f"Could not validate credentials", headers ={"WWW-Authenticate": "Bearer"})
-    return verify_access_token(token, credentials_exception)
+    
+    token = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
