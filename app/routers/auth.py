0d6ade30243a957aee60d141dd919025abf8e519
Dhruv Bajaj <9653dhr@gmail.com>
2021-12-09 12:06:41 +0530

    Update auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -17,10 +17,10 @@ def login(user_cred: OAuth2PasswordRequestForm = Depends(),db: Session = Depends
     user = db.query(models.User).filter(models.User.email == user_cred.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=f"Inavlid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Inavlid Credentials")
 
     if not utils.verify(user_cred.password, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
     
     access_token = oauth2.Create_access_token(data={'user_id' : user.id})
 
